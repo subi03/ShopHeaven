115a96db2b7d85bb31bf679010fbaaf1dfae4d6d
Subiksha <96659807+subi03@users.noreply.github.com>
2023-10-07 11:29:37 +0530

    Update Blob.jsx

--- src/components/Contact/Blob.jsx
+++ src/components/Contact/Blob.jsx
@@ -1,1 +1,7 @@
-
+export default function Blob() {
+  return (
+    <div className={`bg-[url('/blob.svg')] bg-no-repeat hidden md:flex  min-h-[300px] min-w-[300px]
+        flex-col justify-center items-center`}>
+    </div>
+  )
+}
